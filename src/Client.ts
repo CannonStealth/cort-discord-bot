2c5826470bd525fa74b267dcdb00e1268d1070a5
Cannon <73777697+CannonStealth@users.noreply.github.com>
2021-08-11 22:40:12 +0100

    first commit

---
+++ src/Client.ts
@@ -0,0 +1,78 @@
+import { Client as DJSClient, Collection, ClientOptions } from "discord.js";
+import { join } from "path";
+import { readdir, lstat } from "fs/promises";
+import { Client as Bot, key, Command } from "./utils";
+
+class Client extends DJSClient implements Bot {
+
+  public prefix: "-";
+  public commands: Collection<key, Command>;
+  public categories: Collection<key, string[]>;
+  
+  constructor(options: ClientOptions) {
+    super(options);
+    this.prefix = "-";
+    this.commands = new Collection();
+    this.categories = new Collection();
+
+    this.on("messageCreate", (message) => {
+      if (message.author.bot || message.channel.type === "DM") return;
+
+      const { prefix } = this;
+      if (!message.content.startsWith(prefix)) return;
+
+      let args = message.content.split(prefix);
+      args.shift();
+
+      const command =
+        this.commands.find((c) =>
+          args.join(" ").toLowerCase().startsWith(c.name.toLowerCase())
+        ) ||
+        this.commands.find((c) =>
+          c.aliases!
+             .map((a) => a.toLowerCase())
+            .some((b) => args.join(" ").startsWith(b))
+        );
+
+      if (!command) return;
+
+      const toSplit = args.join(" ").startsWith(command.name)
+        ? command.name
+        : command.aliases
+            ?.map((a) => a.toLowerCase())
+            .find((b) => args.join(" ").startsWith(b));
+
+      args = args.join(" ").split(toSplit!).slice(1).join(" ").split(/ +/g);
+      command.run({ client: this, args, message });
+
+      return;
+    });
+  }
+
+  async Commands(dir: string, callback?: Function) {
+    const files = await readdir(join(__dirname, dir));
+    for (const file of files) {
+      const stat = await lstat(join(__dirname, dir, file));
+      if (stat.isDirectory()) this.Commands(join(__dirname, dir));
+      else if (!file.endsWith(".js")) continue;
+
+      const command = require(join(__dirname, dir, file));
+      this.commands.set(command.name.toLowerCase(), command);
+      callback!(command.name, command);
+
+      if (command.category) {
+        let categoryGetter = this.categories.get(
+          command.category.toLowerCase()
+        );
+        if (!categoryGetter) categoryGetter = [command.category.toLowerCase()];
+        categoryGetter.push(command.name.toLowerCase());
+
+        this.categories.set(command.name.toLowerCase(), categoryGetter);
+      }
+    }
+
+    return this;
+  }
+};
+
+export default Client
---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+import { Collection, Message } from "discord.js"
+
+export type Awaited<T> = T | Promise<T>
+export type key = string | number | symbol
+
+type Run = {
+    client: Client;
+    args: string[];
+    message: Message
+} 
+
+export interface Command {
+    name: string;
+    aliases?: string[];
+    category?: string;
+    run: ({}: Run) => Awaited<unknown>
+}
+
+export interface Client {
+    prefix: "-";
+    commands: Collection<key, Command>;
+    categories: Collection<key, string[]>;
+  }
