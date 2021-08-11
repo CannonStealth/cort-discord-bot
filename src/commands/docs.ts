abe63a572b823507c5983423c98dd8809046451e
Cannon <73777697+CannonStealth@users.noreply.github.com>
2021-08-11 23:58:36 +0100

    update docs.ts

--- src/commands/docs.ts
+++ src/commands/docs.ts
@@ -1,29 +1,38 @@
-import axios from "axios";
+import fetch from "node-fetch";
 import { Command } from "../types";
 import { MessageEmbed } from "discord.js";
 
 const reduce = (str: string, int: number) => str.length > int ? str.slice(int - 3) + "..." : str
 export default {
   name: "docs",
-  run({ args, message }) {
-      try {
-    const uri = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
-      args.join(" ")
-    )}`;
+  async run({ args, message }) {
 
-    axios.get(uri)
-      .then(({ data }: any) => {
-        if (data && !data.error) {
-
-            (data as MessageEmbed).fields.map((val) => val.value = reduce(val.value, 1024))
-          message.channel.send({ embeds: [data as MessageEmbed] }).catch(() => undefined)
-        } else {
-          message.reply("Cannot find the documentation you are looking for");
+    const res = await fetch(
+        `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
+          args.join(" ")
+        )}`
+      );
+      const data: MessageEmbed = await res.json();
+      if (!data) {
+        return message.reply("Could not find that!");
+      } else {
+        let embed = new MessageEmbed().setAuthor(
+          "Discord.JS Documentaion",
+          data.author!.iconURL,
+          data.author!.url
+        );
+        if (data.title) {
+          embed.setTitle(data.title);
         }
-      })
-      .catch(() => undefined);
-    } catch {
-        return;
+        if (data.description) {
+          let desc = data.description.replace(/__/g, "");
+          embed.setDescription(desc)
+        }
+        if (data.fields) {
+            data.fields = data.fields.map(field => ({ value: reduce(field.value, 1024), name: field.name, inline: field.inline }))
+          embed.addFields(data.fields);
+        }
+        return message.channel.send({ embeds: [embed] });
+      }
     }
-  },
 } as Command;
